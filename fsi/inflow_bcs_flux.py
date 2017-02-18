3c41586ec4628d5d520bb1ce7a4ad7d4fdaaf7d8
mirok <miroslav.kuchta@gmail.com>
2017-02-18 17:51:21 +0100

    update

--- fsi/inflow_bcs_flux.py
+++ fsi/inflow_bcs_flux.py
@@ -10,70 +10,98 @@ class InflowFromFlux(object):
     a function vector valued function on mesh which has the correct values
     of the 'velocity'.
     '''
-    def __init__(self, mesh, boundaries, marker, n, fluxes):
-        # How we talk to the 3d problem
-        # Let us first compute the 'velocity' as foo on the boundary 
-        bmesh = BoundaryMesh(mesh, 'exterior')
-        c2f = bmesh.entity_map(2)
-        # Marker is where we want to integrate, the rest will be killed of
-        # by bcs
-        bmesh_subdomains = CellFunction('size_t', bmesh, 0)
-        found_markers = set()
-        for cell in cells(bmesh):
-            value = boundaries[c2f[int(cell.index())]]
-            found_markers.add(value)
-            bmesh_subdomains[cell] = value 
-        found_markers = list(found_markers)
-        # In parallel different CPUs have different boundaries
-        comm = bmesh.mpi_comm().tompi4py()
-        found_markers = comm.allreduce(found_markers)
-        found_markers = list(set(found_markers))
-        found_markers.remove(marker)
+    def __init__(self, mesh, boundaries, marker, n, fluxes, source=''):
+        # Load from file
+        if source: 
+            V = VectorFunctionSpace(mesh, 'CG', 1)
+            uh = Function(V)
 
-        V = VectorElement('Lagrange', bmesh.ufl_cell(), 1)
-        Q = FiniteElement('Real', bmesh.ufl_cell(), 0)
-        W = MixedElement([V, Q])
-        W = FunctionSpace(bmesh, W)
+            infile = HDF5File(mesh.mpi_comm(), source, 'r')
+            infile.read(uh, 'uh')
+            infile.close()
 
-        u, p = TrialFunctions(W)
-        v, q = TestFunctions(W)
+            self.uh = uh
+        # Actual computations can for now be done only in serial
+        else:
+            assert mesh.mpi_comm().size == 1
 
-        f = Constant((0, 0, 0))
-        flux = Constant(1)
+            bmesh = BoundaryMesh(mesh, 'exterior')
+            c2f = bmesh.entity_map(2)
+            # Marker is where we want to integrate, the rest will be killed of
+            bmesh_subdomains = CellFunction('size_t', bmesh, 0)
+            found_markers = set()
+            for cell in cells(bmesh):
+                value = boundaries[c2f[int(cell.index())]]
+                found_markers.add(value)
+                bmesh_subdomains[cell] = value 
+            found_markers = list(found_markers)
+            found_markers.remove(marker)    # Now we can kill off
 
-        dx = Measure('dx', domain=bmesh, subdomain_data=bmesh_subdomains)
+            # We solve on a full skeleton
+            V = VectorElement('Lagrange', bmesh.ufl_cell(), 1)
+            Q = FiniteElement('Real', bmesh.ufl_cell(), 0)
+            W = MixedElement([V, Q])
+            W = FunctionSpace(bmesh, W)
 
-        a = inner(grad(u), grad(v))*dx(marker)\
-            + p*inner(v, n)*dx(marker)\
-            + q*inner(u, n)*dx(marker)
-        L = inner(f, v)*dx(marker) + inner(-flux, q)*dx(marker)
+            u, p = TrialFunctions(W)
+            v, q = TestFunctions(W)
 
-        # For bc the cell function must be translated into a facet function.
-        # Bc constrained regions get tag 1
-        bmesh.init(2, 1)
-        facet_f = FacetFunction('size_t', bmesh, 0)
-        for c in chain(*[SubsetIterator(bmesh_subdomains, m) for m in found_markers]):
-            for facet in facets(c): facet_f[facet] = 1
+            f = Constant((0, 0, 0))
+            flux = Constant(1)
 
-        bc = DirichletBC(W.sub(0), Constant((0, 0, 0)), facet_f, 1)
-        # Obtain 'velocity' once than scale it
-        A, b = assemble_system(a, L, bc)
+            dx = Measure('dx', domain=bmesh, subdomain_data=bmesh_subdomains)
 
-        wh = Function(W)
-        solver = LUSolver(A, 'mumps')
-        solver.solve(wh.vector(), b)
+            a = inner(grad(u), grad(v))*dx(marker)\
+                + p*inner(v, n)*dx(marker)\
+                + q*inner(u, n)*dx(marker)
+            L = inner(f, v)*dx(marker) + inner(-flux, q)*dx(marker)
 
-        uh_b, _ = wh.split(deepcopy=True)
-        plot(uh_b, interactive=True)
-        Vb = uh_b.function_space()
-        # We only need the values which are CONSTANT in time
-        array_b = uh_b.vector().get_local()
-        print [v for v in array_b if abs(v) > 1E-10]
+            # For bc the cell function must be translated into a facet function.
+            # Bc constrained regions get tag 1
+            bmesh.init(2, 1)
+            facet_f = FacetFunction('size_t', bmesh, 0)
+            for c in chain(*[SubsetIterator(bmesh_subdomains, m) for m in found_markers]):
+                for facet in facets(c): facet_f[facet] = 1
+            bc = DirichletBC(W.sub(0), Constant((0, 0, 0)), facet_f, 1)
+           
+            A, b = assemble_system(a, L, bc)
+            wh = Function(W)
+            
+            solver = LUSolver(A, 'mumps')
+            print 'System size is', A.size(0)
+            solver.solve(wh.vector(), b)
 
-        # Now we a way to talk to the world
-        V = VectorFunctionSpace(mesh, 'CG', 1)
-        uh = Function(V)
-        array = uh.vector().get_local()  # Alloc
+            # plot(wh, interactive=True)
+
+            uhb, _ = wh.split(deepcopy=True)
+            Vb = uhb.function_space()
+
+            # Okay now we need to extend to full mesh
+            V = VectorFunctionSpace(mesh, 'CG', 1)
+            uh = Function(V)
+            
+            # We want to transer values from boundary space to space
+            dofb_vb = np.array(dof_to_vertex_map(Vb), dtype=int)
+            vb_v = np.array(bmesh.entity_map(0), dtype=int)
+            v_dof = np.array(vertex_to_dof_map(V), dtype=int)
+
+            array = uh.vector().array()
+            in_array = uhb.vector().get_local()
+
+            dim = nsubs = 3
+            vb_v = np.repeat(vb_v, dim)
+            maps = []
+            for i in range(nsubs):
+                maps.append(v_dof[i::dim][vb_v[dofb_vb[i::dim]]])
+            mapping = np.array(sum(map(list, zip(*maps)), []))
+
+            array[mapping] = in_array
+            uh.vector().set_local(array)
+            uh.vector().apply('insert')
+
+            ofile = HDF5File(mesh.mpi_comm(), 'inflow_%d.h5' % mesh.num_cells(), 'w')
+            ofile.write(uh, 'uh')
+            ofile.close()
 
         # Next we would like to have a mapping for how to update values of uh
         # based on uh_b based on time dependent flux values
@@ -81,23 +109,8 @@ class InflowFromFlux(object):
         self.times = fluxes[:, 0]
         self.fluxes = fluxes[:, 1]
         self.period = self.times[-1]
-        self.uh = uh
-
-        # THIS should work in parallel. For now it does not even work in serial
-        # FIXME
-        dofb_vb = np.array(dof_to_vertex_map(Vb), dtype=int)
-        vb_v = np.repeat(np.array(bmesh.entity_map(0), dtype=int), 3)
-        v_dof = np.array(vertex_to_dof_map(V), dtype=int)
-
-        def _update_impl(t, (t0, t1), (q0, q1)):
-            qt = q1*(t-t0)/(t1-t0) + q0*(t-t1)/(t1-t0)
-            print t, qt,
-            array[v_dof[vb_v[dofb_vb]]] = array_b*qt
-            print max(np.abs(array_b*qt)), max(np.abs(array)), '><', id(array), v_dof[vb_v[dofb_vb]],
-            uh.vector().set_local(array)
-            uh.vector().apply('insert')
-            print uh.vector().norm('l2')
-        self._update = _update_impl
+        self.uh = uh  # The interface, values changed in time
+        self.U = uh.vector().get_local()  # Values constant in time
 
         self.t = 0.
 
@@ -119,8 +132,13 @@ class InflowFromFlux(object):
                  if between(self._time, (times[i], times[i+1]))].pop()
         (t0, t1) = times[index], times[index+1]
         (q0, q1) = fluxes[index], fluxes[index+1]
-        # Now update the values of f
-        self._update(t, (t0, t1), (q0, q1))
+
+        # Now update the values of uh
+        qt = (q1*(t-t0)/(t1-t0) + q0*(t1-t)/(t1-t0))
+
+        Uqt = self.U*qt
+        self.uh.vector().set_local(Uqt)
+        self.uh.vector().apply('insert')
 
 # ----------------------------------------------------------------------------
 
@@ -144,7 +162,8 @@ if __name__ == '__main__':
 
     inflow = InflowFromFlux(mesh,
                             boundaries, marker=1, n=Constant((-1, 0, 0)),
-                            fluxes=fluxes)
+                            fluxes=fluxes,
+                            source='inflow_72415.h5')
     inflow_f = inflow.uh
 
     bmesh = BoundaryMesh(mesh, 'exterior')
@@ -159,7 +178,7 @@ if __name__ == '__main__':
         L = inner(zero, v)*dx
 
         bci = [DirichletBC(V, inflow_f, boundaries, 1)]
-        bc0 = [DirichletBC(V, zero, boundaries, i) for i in (4, 2, 3)]
+        bc0 = [DirichletBC(V, zero, boundaries, i) for i in (0, 4, 2, 3)]
         bcs = bc0 + bci
 
         assembler = SystemAssembler(a, L, bcs)
